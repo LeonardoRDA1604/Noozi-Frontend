bf6ec8599239bbbc138707979685c7909b44ba37
Ggeasy1574 <gabrielluck492@gmail.com>
2026-05-13 12:44:20 -0300

    feat(ProductFilterButton): create FilterButton file and Searchbar folder

---
+++ src/components/Buttons/FilterProductButton.tsx
@@ -0,0 +1,13 @@
+import { Funnel } from "lucide-react"
+
+export default function FilterProductButton(){
+   return (
+    <>
+    <div>
+        <button className="mt-500">
+            <Funnel/>
+        </button>
+    </div>
+    </>
+   ) 
+}
