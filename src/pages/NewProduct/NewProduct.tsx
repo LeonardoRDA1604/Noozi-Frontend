558bf415178b7999ffaa4bcb82c33c2956d2aebd
LeonardoRDA1604 <leonardo.rafael1604@gmail.com>
2026-05-18 22:22:24 -0300

    refactor(new-product): fix merge inconsistencies in the New Product page

--- src/pages/NewProduct/NewProduct.tsx
+++ src/pages/NewProduct/NewProduct.tsx
@@ -1,39 +1,10 @@
 // import { Send } from "lucide-react";
 // import { ActionButton } from "@/components/Buttons/ActionButton/ActionButton";
 // import { useProductMetrics } from "@/hooks/useProductMetrics";
-
-// export default function NewProduct() {
-//   const { isLoading } = useProductMetrics();
-//   return (
-//     <>
-//       <div className="p-4">
-//         <h1 className="flex items-center justify-center text-4xl font-bold text-noozi-bright_blue">
-//           Noozi/new-product-page
-//         </h1>
-//       </div>
-
-//       {/* Div do botão de submit */}
-//       <div className="flex flex-col gap-2 my-6 max-w-xs">
-//         <ActionButton
-//           variant="submit"
-//           icon={Send}
-//           label="Cadastrar produto"
-//           isLoading={isLoading}
-//         />
-//       </div>
-//     </>
-//   );
-// }
-
-
-
-
-
-
-
 import { useState } from "react";
 
 export default function Form() {
+  // const { isLoading } = useProductMetrics();
   const [name, setName] = useState("");
   const [brand, setBrand] = useState("");
   const [description, setDescription] = useState("");
@@ -398,3 +369,13 @@ const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     </div>
   );
 }
+
+      // {/* Div do botão de submit */}
+      // <div className="flex flex-col gap-2 my-6 max-w-xs">
+      //   <ActionButton
+      //     variant="submit"
+      //     icon={Send}
+      //     label="Cadastrar produto"
+      //     isLoading={isLoading}
+      //   />
+      // </div>
