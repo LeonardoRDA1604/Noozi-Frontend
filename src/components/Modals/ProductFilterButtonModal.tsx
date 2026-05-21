b86e2088640268853722043207cc9b80d8630290
Gabriel Xavier <gabrielluck492@gmail.com>
2026-05-21 18:33:06 -0300

    feat(product-filter-modal): create sections to each option-type of sorting

--- src/components/Modals/ProductFilterButtonModal.tsx
+++ src/components/Modals/ProductFilterButtonModal.tsx
@@ -2,36 +2,61 @@ import { ActionButton } from "../Buttons/ActionButton/ActionButton";
 import { CheckIcon } from "lucide-react";
 
 interface ModalProps {
-    isOpen : boolean;
-    onClose : () => void;
+    isOpen: boolean;
+    onClose: () => void;
 }
 
-export default function FilterButtonModal ({isOpen, onClose} : ModalProps) {
-    
-    if(!isOpen) return null;
+
+
+export default function FilterButtonModal({ isOpen, onClose }: ModalProps) {
+
+    if (!isOpen) return null;
 
     return (
         <div className="relative">
             <div className="fixed top-0 left-0 w-[100%] h-[100%] flex items-center justify-center bg-gray-300 overflow-y-auto overscroll-none overflow-hidden">
                 <div className="bg-white rounded-md w-[30em] shadow-lg py-[2rem]">
                     <div className="flex justify-end text-lg pr-5">
-                        <button className="h-6" onClick={onClose}> 
+                        <button className="h-6" onClick={onClose}>
                             &times;
-                        </button>                
+                        </button>
                     </div>
 
-                    <div className=" flex justify-center mb-5 gap-5">
-                        <h2>Selecione o Filtro</h2>
-                        <select name="filtros" id="ProductFilters" className="text-center bg-gray-200 rounded-md">
-                            <option value="Alphabetic Sorted Asc">A-Z</option>
-                            <option value="Alphabetic Sorted Desc">Z-A</option>
-                            <option value="Brand">Marca</option>
-                            <option value="Newest">Mais Recente</option>
-                            <option value="Oldest">Mais Antigo</option>
-                            <option value="Expensivest">Maior Preço</option>
-                            <option value="Cheapest">Menor Preço</option>
-                            <option value="SKU">SKU</option>
-                        </select>
+                    {/* Separa as seções de cada uma das opções de ordenamento dos itens na tela*/}
+                    <div id="OrderBySection"> 
+                        <label htmlFor="">Ordenar Por:</label>
+                        <div id="OrderByText">
+                            <label htmlFor="">Ordem alfabética</label>
+                            <button>A-Z</button>
+                            <button>Z-A</button>
+                        </div>
+                        <div id="OrderByStockDisponibility">
+                            <label htmlFor="">Estoque</label>
+                            <button>Maior Estoque</button>
+                            <button>Menor Estoque</button>
+                        </div>
+                        <div id="OrderByExpireTime">
+                            <label htmlFor="">Validade</label>
+                            <button>Mais próximo à validade</button>
+                            <button>Mais longe à validade</button>
+                        </div>
+                        <div id="OrderByIsActive">
+                            <label htmlFor="">Somente Ativos</label>
+                            <input type="checkbox" name="" id="" value="Sim" />
+                        </div>
+                        <div id="FilterByPrice">
+                            <label htmlFor="">Faixa de Preço</label>
+                            <div>
+                                <div>
+                                    <label htmlFor="">De:</label>
+                                    <input type="text" name="" id="" placeholder="Ex: 200" />
+                                </div>
+                                <div>
+                                    <label htmlFor="">Para:</label>
+                                    <input type="text" name="" id="" placeholder="Ex: 600" />
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
                     <div className="flex justify-evenly">
@@ -42,7 +67,7 @@ export default function FilterButtonModal ({isOpen, onClose} : ModalProps) {
                             onSuccess={() => {
                                 onClose() // fecha o modal
                             }}
-                            />                                     
+                        />
                     </div>
                 </div>
             </div>
